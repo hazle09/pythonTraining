5d639e3710486b222ba1e0bf673a82cd8788b94a
hazle09 <joohyenim@gmail.com>
2020-03-25 23:01:23 +0900

    웹크롤링^^

---
+++ 웹크롤링.py
@@ -0,0 +1,12 @@
+from urllib import parse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+keyword='한국어'
+url='https://ko.wikipedia.org/wiki/'+parse.quote(keyword)
+source=BeautifulSoup(urlopen(url),'html.parser')
+target=source.find_all('li',{'class':'toclevel-1'})
+body=source.find('div',{'id':'bodyContent'})
+paragraphs=body.find_all('p')
+for p in paragraphs:
+    print(p.get_text().strip())
+    break
